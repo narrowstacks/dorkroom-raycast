f57f50ccd7062b4ed5e0b9b262bd111693d5b17d
narrowstacks <aaron@affords.art>
2025-06-06 16:48:29 -0700

    Update .gitignore to include log files, environment variables, and editor configurations; add new dependencies in package-lock.json; update package.json to correct icon filenames and remove unused assets.

--- src/easel-calculator.tsx
+++ src/easel-calculator.tsx
@@ -1,6 +1,4 @@
-import { List, ActionPanel, Action, Icon } from "@raycast/api";
 import { TemplateList } from "./components/border-calculator";
-import { TemplateForm } from "./components/border-calculator/TemplateForm";
 
 export default function Command() {
   return <TemplateList />;
